18c6032c6de0d02135efc7882b5a90640ea42995
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-25 12:08:48 -0700

    Create app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'root',
+  template: `<h1>This is my first component</h1>`,
+  styleUrls: ['app.component.css']
+})
+export class AppComponent { }
